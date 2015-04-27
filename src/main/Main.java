e13daaf62e41df8191ddf087ace63c802bf81131
batmaster <batmaster_kn@hotmail.com>
2015-04-27 15:38:28 +0700

    Add GUI (MainFrame.java) Add guideance how to call GUI in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.xml.ws.WebServiceException;
+
+import view.MainFrame;
 import net.webservicex.*;
 
 public class Main {
@@ -14,5 +19,32 @@ public class Main {
 		
 		String country = proxy.getCountryByCountryCode("TH");
 		System.out.println(THISD);
+		
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.run();
+		
+		/* How to call GUI is here
+		while (true) {
+			try {
+				mainFrame.showStatus("Connecting...");
+				FrequencyUnit factory = new FrequencyUnit();
+				FrequencyUnitSoap proxy = factory.getFrequencyUnitSoap();
+				
+				mainFrame.setSoap(proxy);
+				break;
+			} catch (WebServiceException e) {
+				mainFrame.showStatus("Connection error");
+				e.printStackTrace();
+				
+				JDialog.setDefaultLookAndFeelDecorated(true);
+			    int response = JOptionPane.showConfirmDialog(null, "YES to check again or NO to close.", "Connection error!", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+			    
+			    if (response == JOptionPane.NO_OPTION || response == JOptionPane.CLOSED_OPTION) {
+			    	System.exit(0);
+			    	break;
+			    }
+			}
+		}
+		*/ 
 	}
 }
